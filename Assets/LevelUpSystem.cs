52ce9384a0ff7ae14010241ca2d9f0c83cdfcce3
Paul Ollivier <p0l@me.com>
2019-11-05 21:26:23 +0100

    First Commit

---
+++ Assets/LevelUpSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+
+public class LevelUpSystem : ComponentSystem
+{
+   protected override void OnUpdate()
+   {
+       // Entities.With(EntityQuery).ForEach( (Entity e, ref MyComponent c) => { /*do something*/ });
+       Entities.ForEach( (Entity levelComponent, ref LevelComponent c) =>
+       {
+           levelComponent.level += 1;
+       });
+
+   }
+}
---
+++ Assets/Testing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using Unity.Entities;
+
+public class Testing : MonoBehaviour
+{
+    private void Start()
+    {
+        var entityManager = World.Active.EntityManager;
+        var entity = entityManager.CreateEntity(typeof(LevelComponent));
+
+        entityManager.SetComponentData(entity, new LevelComponent { level = 10 });
+    }
+
+    void Update()
+    {
+        
+    }
+}
