7c0188ba542ac14a01aa43677af6ff7864bb1f52
Paul Ollivier <p0l@me.com>
2019-11-05 23:16:48 +0100

    Rotate example
    + using Jobs

---
+++ Assets/01_Rotating/Scripts/RotateAuthoring.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+using static Unity.Mathematics.math;
+
+public class RotateAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    [SerializeField] private float degreesPerSecond;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new Rotate { radiansPerSecond = Unity.Mathematics.math.radians(degreesPerSecond)});
+        dstManager.AddComponentData(entity, new RotationEulerXYZ());
+    }
+}
---
+++ Assets/01_Rotating/Scripts/RotateSystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Jobs;
+using Unity.Entities;
+using Unity.Burst;
+using Unity.Transforms;
+using UnityEngine;
+
+public class RotateSystem : JobComponentSystem
+{
+    [BurstCompile]
+    private struct RotateJob : IJobForEach<RotationEulerXYZ, Rotate>
+    {
+        public float deltaTime;
+
+        public void Execute(ref RotationEulerXYZ euler, ref Rotate rotate)
+        {
+            euler.Value += rotate.radiansPerSecond * deltaTime;
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new RotateJob{ deltaTime = Time.deltaTime};
+        return job.Schedule(this, inputDeps);
+    }
+}
